a9db5c1f83318650260ccc6f2f2d5c5ab0452ec5
PramodShenoy <pramodhthegreats@gmail.com>
2018-05-03 14:20:31 +0530

    post changes

--- shop/src/main/java/PaymentController.java
+++ shop/src/main/java/PaymentController.java
@@ -1,10 +1,11 @@
 package shop;
-
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Null;
 
 @RestController
 public class PaymentController {
@@ -15,7 +16,7 @@ public class PaymentController {
     private static final String ERROR_STATUS = "error";
     private static final int CODE_SUCCESS = 100;
     private static final int AUTH_FAILURE = 102;
-    private shop.PaymentRequest re = new shop.PaymentRequest();
+    private shop.PaymentRequest re=null;
     @RequestMapping(value = "/pay", method = RequestMethod.POST)
     public shop.PaymentResponse pay(@RequestParam(value = "key") String key, @RequestBody shop.PaymentRequest request) {
 
@@ -25,6 +26,7 @@ public class PaymentController {
             int userId = request.getUserId();
             String itemId = request.getItemId();
             double discount = request.getDiscount();
+            re = new shop.PaymentRequest();
             re.setUserId(userId);
             re.setDiscount(discount);
             re.setItemId(itemId);
@@ -49,10 +51,14 @@ public class PaymentController {
     @RequestMapping(value="/value",method = RequestMethod.GET)
     public double value()
     {
-        System.out.println("----------------------"+re.getItemId()+"-----------------------");
-        System.out.println("----------------------"+re.getUserId()+"-----------------------");
-        System.out.println("----------------------"+re.getDiscount()+"-----------------------");
-        double val = 1000-re.getDiscount()*Double.parseDouble(re.getItemId());
-        return val;
+        if(re!=null) {
+            System.out.println("----------------------" + re.getItemId() + "-----------------------");
+            System.out.println("----------------------" + re.getUserId() + "-----------------------");
+            System.out.println("----------------------" + re.getDiscount() + "-----------------------");
+            double val = 1000 - re.getDiscount() * Double.parseDouble(re.getItemId());
+            return val;
+        }
+        else
+            return 0.0;
     }
 }
