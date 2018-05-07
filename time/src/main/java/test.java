f4b517ba3066c3e6c44e51bbfcf1d8d2ec1ef66c
PramodShenoy <pramodhthegreats@gmail.com>
2018-05-07 17:32:35 +0530

    updates in time

--- time/src/main/java/test.java
+++ time/src/main/java/test.java
@@ -1,4 +1,5 @@
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -7,8 +8,17 @@ public class test
 {
    public static void main(String args[])
    {
-       SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+   /*    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
        System.out.println("Date Time : "+ sdf.format(new Date()));
+       long unixTimestamp = Instant.now().getEpochSecond();
+       System.out.println(unixTimestamp);
+       System.out.println("Date Time2 : "+ sdf.format(unixTimestamp));*/
+       long unixSeconds = Instant.now().getEpochSecond();
+       Date date = new java.util.Date(unixSeconds*1000L);
+       SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+       //sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT-4"));
+       String formattedDate = sdf.format(date);
+       System.out.println(formattedDate);
    }
 
 }
