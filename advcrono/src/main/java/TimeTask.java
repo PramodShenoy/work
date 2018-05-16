025764d78e6a3362f69c4f65f107cffc2350e8ac
PramodShenoy <pramodhthegreats@gmail.com>
2018-05-16 17:52:15 +0530

    added advanced scheduling thing^C

---
+++ advcrono/src/main/java/TimeTask.java
@@ -0,0 +1,51 @@
+package advcrono;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@Controller
+public class TimeTask
+{
+    //http://www.quartz-scheduler.org/documentation/quartz-2.x/tutorials/crontrigger.html for cron expression
+    private static final Logger logger = LoggerFactory.getLogger(TimeTask.class);
+
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+    @Scheduled(fixedRate = 2000)
+    public void scheduleTaskWithFixedRate() {
+        logger.info("Fixed Rate Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()) );
+    }
+
+    @Scheduled(fixedDelay = 2000)
+    public void scheduleTaskWithFixedDelay() {
+        logger.info("Fixed Delay Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException ex) {
+            logger.error("Ran into an error {}", ex);
+            throw new IllegalStateException(ex);
+        }
+    }
+
+    @Scheduled(fixedRate = 2000, initialDelay = 5000)
+    public void scheduleTaskWithInitialDelay() {
+        logger.info("Fixed Rate Task with Initial Delay :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
+    }
+
+    @Scheduled(cron = "0 * * * * ?")
+    public void scheduleTaskWithCronExpression() {
+        logger.info("Cron Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
+    }
+
+}
