2097b95f498234e6acef89c43cca77e1f243ff64
PramodShenoy <pramodhthegreats@gmail.com>
2018-06-12 17:24:24 +0530

    insert,update,calc done

--- project/src/main/java/com/app/MyController.java
+++ project/src/main/java/com/app/MyController.java
@@ -1,12 +1,9 @@
 package com.app;
 
 
-import com.app.enums.FilingFrequencyEnum;
-import com.app.enums.TaxTypeEnum;
-import com.cf.public_.tables.Tax;
+import com.cf.public_.tables.records.FilingsRecord;
 import lombok.extern.slf4j.Slf4j;
 import org.jooq.DSLContext;
-import org.jooq.Result;
 import org.jooq.impl.DSL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,15 +20,15 @@ import java.util.Map;
 import static com.cf.public_.Tables.FILINGS;
 import static com.cf.public_.Tables.TAX;
 
-
 @RestController
 @Slf4j
 public class MyController {
     @Autowired
     private CalculationService calculationService;
     @Autowired
-    private DataSource dataSource;
-    private DSLContext dslContext;
+    private InsertDataService insertDataService;
+    @Autowired
+    private UpdateService updateService;
 
     @RequestMapping(value = "/get", method = RequestMethod.POST)
     public ResponseEntity<String> get(@RequestBody Request request)
@@ -48,35 +45,16 @@ public class MyController {
     @RequestMapping(value = "/insert",method = RequestMethod.POST)
     public ResponseEntity<String> insert(@RequestBody DBEntry dbEntry)
     {
-        try {
-            dslContext = DSL.using(dataSource.getConnection());
-            Result result = dslContext.select(TAX.ID)
-                    .from(TAX)
-                    .where(TAX.ID.eq(dbEntry.getId()))
-                    .fetch();
-            if (result.isEmpty())
-            {
-                dslContext.insertInto(TAX,TAX.APP_ID,TAX.STATE,TAX.FROM_YEAR,TAX.TO_YEAR,TAX.TAX_TYPE,TAX.FILING_FREQENCY)
-                        .values(dbEntry.getApp_id(),dbEntry.getState(),dbEntry.getFrom_year(),dbEntry.getTo_year(),
-                                TaxTypeEnum.valueOf(dbEntry.getTax_type()).id(),FilingFrequencyEnum.valueOf(dbEntry.getFiling_frequency()).id())
-                        .execute();
-                Result<?> result1=dslContext.select(TAX.ID).from(TAX).orderBy(TAX.ID.desc()).fetch();
-                int id =(Integer)result1.getValue(0,"id");
-                log.info("ID LAST " + id);
-                dslContext.insertInto(FILINGS,FILINGS.ID,FILINGS.JAN,FILINGS.FEB,FILINGS.MAR,FILINGS.APR,FILINGS.MAY,
-                        FILINGS.JUN,FILINGS.JUL,FILINGS.AUG,FILINGS.SEP,FILINGS.OCT,FILINGS.NOV,FILINGS.DEC)
-                        .values(id,BigDecimal.valueOf(dbEntry.getJan()),BigDecimal.valueOf(dbEntry.getFeb()),BigDecimal.valueOf(dbEntry.getMar()),
-                                BigDecimal.valueOf(dbEntry.getApr()),BigDecimal.valueOf(dbEntry.getMay()),BigDecimal.valueOf(dbEntry.getJun()),
-                                BigDecimal.valueOf(dbEntry.getJul()),BigDecimal.valueOf(dbEntry.getAug()),BigDecimal.valueOf(dbEntry.getSep()),
-                                BigDecimal.valueOf(dbEntry.getOct()),BigDecimal.valueOf(dbEntry.getNov()),BigDecimal.valueOf(dbEntry.getDec()))
-                        .execute();
-            }
-            log.info("INSERTED SUCCESSFULLY IN BOTH TABLES");
+        if(insertDataService.insertData(dbEntry)==0)
             return new ResponseEntity<>("DONE",HttpStatus.OK);
-        }catch (Exception e)
-        {
-            log.error("ERROR IN INSERTING " + e);
-            return new ResponseEntity<>("ERROR IN INSERTING",HttpStatus.OK);
-        }
+        return new ResponseEntity<>("ERROR IN INSERTING",HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/update",method = RequestMethod.POST)
+    public ResponseEntity<String> update(@RequestBody DBEntry dbEntry)
+    {
+        if(updateService.updateFilingsData(dbEntry)==0)
+            return new ResponseEntity<>("UPDATE DONE",HttpStatus.OK);
+        return new ResponseEntity<>("ERROR IN FINDING ROW",HttpStatus.OK);
     }
 }
---
+++ project/src/main/java/com/app/UpdateService.java
@@ -0,0 +1,51 @@
+package com.app;
+
+import com.cf.public_.tables.records.FilingsRecord;
+import lombok.extern.slf4j.Slf4j;
+import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+import java.math.BigDecimal;
+
+import static com.cf.public_.Tables.FILINGS;
+
+@Component
+@Slf4j
+public class UpdateService {
+    @Autowired
+    private DataSource dataSource;
+    private DSLContext dslContext;
+
+    public int updateFilingsData(DBEntry dbEntry)
+    {
+        try {
+            dslContext = DSL.using(dataSource.getConnection());
+            FilingsRecord filingsRecord = dslContext.selectFrom(FILINGS).where(FILINGS.ID.eq(dbEntry.getId())).fetchOne();
+            if(filingsRecord!=null)
+            {
+                filingsRecord.setJan(BigDecimal.valueOf(dbEntry.getJan()));
+                filingsRecord.setFeb(BigDecimal.valueOf(dbEntry.getFeb()));
+                filingsRecord.setMar(BigDecimal.valueOf(dbEntry.getMar()));
+                filingsRecord.setApr(BigDecimal.valueOf(dbEntry.getApr()));
+                filingsRecord.setMay(BigDecimal.valueOf(dbEntry.getMay()));
+                filingsRecord.setJun(BigDecimal.valueOf(dbEntry.getJun()));
+                filingsRecord.setJul(BigDecimal.valueOf(dbEntry.getJul()));
+                filingsRecord.setAug(BigDecimal.valueOf(dbEntry.getAug()));
+                filingsRecord.setSep(BigDecimal.valueOf(dbEntry.getSep()));
+                filingsRecord.setOct(BigDecimal.valueOf(dbEntry.getOct()));
+                filingsRecord.setNov(BigDecimal.valueOf(dbEntry.getNov()));
+                filingsRecord.setDec(BigDecimal.valueOf(dbEntry.getDec()));
+                filingsRecord.store();
+                return 0;
+            }
+        }catch (Exception e)
+        {
+            log.error("ERROR " + e);
+        }
+        return -1;
+    }
+}
